b4d57906a6ca928918b557dad4a92b6e23389c11
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-11-01 23:53:33 -0700

    Add test spec scaffolding

---
+++ server/spec/database/02games.js
@@ -0,0 +1,19 @@
+var request = require('request');
+var expect = require('chai').expect;
+
+describe('Games', function() {
+  it("should post a new game", function(done) {
+  });
+
+  it("should get an existing game's summary", function(done) {
+  });
+
+  it("should get a list of games played by a specific duo", function(done) {
+  });
+
+  it("should post a comment for a specific game", function(done) {
+  });
+
+  it("should get a list of comments for a specific game", function(done) {
+  });
+});
