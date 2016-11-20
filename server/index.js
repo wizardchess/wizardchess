e917419f9d5d289c283b2ff2a750fe9806260cdc
Joann Im <joann_im@hotmail.com>
2016-11-20 14:36:58 -0800

    Install Sequelize and connect server to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const Sequelize = require('sequelize');
+ 
+const sequelize = new Sequelize('wizardchess', '', '', {
+  dialect: 'postgres'
+});
 
+sequelize.sync()
+		 .then(() => console.log('We are connected.'))
+		 .catch(err => console.log(`Unable to connect with the following error: ${err}`));
+		 
+//sync connects the server (node) with the database
 app.use(express.static(__dirname + '/../public'));
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
