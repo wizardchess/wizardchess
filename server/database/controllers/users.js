dbc32f646abfda2f7114d18e07cc06062b05e4ea
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-11-21 01:30:23 -0800

    Fix Sequelize findOne query to return null from invalid input

--- server/database/controllers/users.js
+++ server/database/controllers/users.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 const User = require('./../models/users')
 
 const getUserByQuery = (req, res) => {
-  User.findOne(req.query)
+  console.log(req.query);
+  User.findOne({ where: req.query })
       .then((user) => {
         res.json(user);
       })
