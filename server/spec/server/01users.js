ed0457e22cdd5744108eb3e6b8289b82c51a92cd
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-11-21 00:09:18 -0800

    Moved database examples to its own file

--- server/spec/server/01users.js
+++ server/spec/server/01users.js
@@ -1,42 +1,21 @@
+const Sequelize = require('sequelize');
+const User = require('./../../database/models/users');
 var request = require('request');
 var expect = require('chai').expect;
+var { Harry, Malfoy } = require('./dbExamples');
 
 describe('Users', function() {
   // before
   beforeEach(() => {
-    const Harry = {
-      id: 1,
-      username: 'boywholived',
-      password: 'expectopatronus',
-      facebook_id: 'boywholived',
-      first_name: 'Harry',
-      last_name: 'Potter',
-      avatar_id: 1980,
-      birthday: Date.now(),
-      date_time: Date.now(),
-    }
-
-    const Malfoy = {
-      id: 1,
-      username: 'pureblood',
-      password: 'ihateharry',
-      facebook_id: 'pureblood',
-      first_name: 'Draco',
-      last_name: 'Malfoy',
-      avatar_id: 1981,
-      birthday: Date.now(),
-      date_time: Date.now(),
-    }
-
-    const QuidditchCup = {
-      id: 1,
-      user_id: 1,
-      achievement_name: 'Quidditch Cup',
-      date_time: Date.now(),
-    }
-  });
     // create test database
+    const database = new Sequelize('test', '', '', {
+      dialect: 'postgres'
+    });
+
     // add harry potter user record
+    User.create(Harry)
+        .then((user) => console.log(user.first_name, user.last_name));
+  });
     // add malfoy user record
     // add harry vs malfoy game record
     // add malfoy vs harry game record
@@ -45,6 +24,7 @@ describe('Users', function() {
 
   afterEach(() => {
     // destroy all rows
+    User.destory();
   });
 
   it("should get a user's profile", function(done) {
