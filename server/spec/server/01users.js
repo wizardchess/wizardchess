94312038faeebe73acb8af6681eb23af025a6c4c
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-11-21 00:01:55 -0800

    Add character examples

--- server/spec/server/01users.js
+++ server/spec/server/01users.js
@@ -3,6 +3,38 @@ var expect = require('chai').expect;
 
 describe('Users', function() {
   // before
+  beforeEach(() => {
+    const Harry = {
+      id: 1,
+      username: 'boywholived',
+      password: 'expectopatronus',
+      facebook_id: 'boywholived',
+      first_name: 'Harry',
+      last_name: 'Potter',
+      avatar_id: 1980,
+      birthday: Date.now(),
+      date_time: Date.now(),
+    }
+
+    const Malfoy = {
+      id: 1,
+      username: 'pureblood',
+      password: 'ihateharry',
+      facebook_id: 'pureblood',
+      first_name: 'Draco',
+      last_name: 'Malfoy',
+      avatar_id: 1981,
+      birthday: Date.now(),
+      date_time: Date.now(),
+    }
+
+    const QuidditchCup = {
+      id: 1,
+      user_id: 1,
+      achievement_name: 'Quidditch Cup',
+      date_time: Date.now(),
+    }
+  });
     // create test database
     // add harry potter user record
     // add malfoy user record
@@ -10,6 +42,10 @@ describe('Users', function() {
     // add malfoy vs harry game record
     // add house cup achievement record
     // add graduation achievement record
+
+  afterEach(() => {
+    // destroy all rows
+  });
 
   it("should get a user's profile", function(done) {
   });
