e5583c924204ca3500fa92176ab7f997487e2e43
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-11-20 22:17:15 -0800

    Remove comments from sequelize configuration script

--- server/database/sequelize.js
+++ server/database/sequelize.js
@@ -3,12 +3,6 @@ const database = new Sequelize('wizardchess', '', '', {
   dialect: 'postgres'
 });
 
-// database.sync()
-// 		 .then(() => console.log('We are connected.'))
-// 		 .catch(err => console.log(`Unable to connect with the following error: ${err}`));
-
-//sync connects the server (node) with the database
-
 module.exports = {
   Sequelize,
   database
