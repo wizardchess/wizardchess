8fbcefdcdff023675de9ad7d82bf9a5e952b3ee4
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-11-21 01:33:06 -0800

    Remove query console log

--- server/database/controllers/users.js
+++ server/database/controllers/users.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 const User = require('./../models/users')
 
 const getUserByQuery = (req, res) => {
-  console.log(req.query);
   User.findOne({ where: req.query })
       .then((user) => {
         res.json(user);
