4ce190a674a7032cb847eaeb014fc6b8534057d6
Jeremiah Arella <jeremiah.arella@gmail.com>
2016-11-21 13:48:12 -0800

    Add controllers to register and get all users

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { getUserByQuery } = require('./../database/controllers/users');
+const { getUserByQuery, registerNewUser, getAllUsers } = require('./../database/controllers/users');
 
 router.get('/users', (req, res) => {
   getUserByQuery(req, res);
+});
+
+router.post('/users', (req, res) => {
+  registerNewUser(req, res);
+});
+
+router.get('/allusers', (req, res) => {
+  getAllUsers(req, res);
 });
 
 module.exports = router;
